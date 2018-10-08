3237cba6566259d5ea8f7bc3e5f0229185978693
cjsjyh <jyhckb@naver.com>
2018-10-08 08:46:06 +0000

    Modified Outputs

--- main.c
+++ main.c
@@ -2,17 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
+char **memory;
+void cmd_dump(int, int);
+
 int main()
 {
 	int q_flag = 1;
 	char command[30],arg1[20],arg2[20],arg3[20];
-	char **memory;
+	
 	
 	memory=(char**)malloc(sizeof(char*)*65536);
 	for (int i=0; i<65536; i++)
 	{
 		memory[i] = (char*)malloc(sizeof(char)*16);
-		memset(memory[i],0,sizeof(char)*16);
+		memset(memory[i],16,sizeof(char)*16);
 	}
 	
 	while(q_flag)
@@ -21,9 +24,25 @@ int main()
 		scanf("%s",command);
 		//DUMP!
 		if (!strcmp(command,"du") || !strcmp(command,"dump"))
-			for(int i=0; i<10; i++){
-				for(int j=0; j<16; j++)
-					printf("%02x ",memory[i][j]);
-				printf("\n");
-	}		}
+			cmd_dump(0,2);
+	}
+}
+
+void cmd_dump(int start, int end)
+{
+	for(int i=0; i<10; i++){
+		printf("%05x  ",start + 16*i);
+		for(int j=0; j<16; j++)
+			printf("%02x ",memory[i][j]);
+		
+		for(int j=0; j<16; j++)
+		{
+			if (memory[i][j] >= 20 && memory[i][j] <= 0x7E){
+				printf("%c ",memory[i][j]);
+			} else{
+				printf(". ");
+			}
+		}
+		printf("\n");
+	}
 }
